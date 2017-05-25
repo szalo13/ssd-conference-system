54d1efdf54ebdaf0e42307a36c9880126dec1002
szalo13 <kamil_sza@yahoo.pl>
2017-05-25 18:44:11 +0200

    Adding event emiter, changing button type

--- frontend/src/components/login-form/login-form.ts
+++ frontend/src/components/login-form/login-form.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Output, EventEmitter } from '@angular/core';
 import { Angular2TokenService, SignInData } from "angular2-token";
 import { Http } from '@angular/http';
 import { environment } from "../../app/environments/environment";
@@ -17,8 +17,12 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class LoginFormComponent {
 
-  signInData: SignInData = <SignInData>{};
   form: FormGroup;
+  signInData = {
+    email: '',
+    password: ''
+  }
+  @Output() onFormResult = new EventEmitter<any>();
 
   constructor(
     private authToken: Angular2TokenService,
@@ -32,20 +36,18 @@ export class LoginFormComponent {
       this.authToken.init(environment.token_auth_config);
   }
 
-  private signIn() {
-    this.authToken.signIn({
-      email: this.signInData.email,
-      password: this.signInData.password
-    }).subscribe(
+  private onSignInSubmit() {
+    this.authToken.signIn(this.signInData).subscribe(
       res => {
-
-        console.log('auth response:', res);
-        console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-        console.log('auth response body:', res.json()); //log the response body to show the user
+        if(res.status == 200) {
+          console.log("logged in");
+          this.onFormResult.emit({signedIn: true, res});
+        }
       },
 
       err => {
         console.error('auth error:', err);
+        this.onFormResult.emit({signedIn: false, err});
       }
   )
   }
