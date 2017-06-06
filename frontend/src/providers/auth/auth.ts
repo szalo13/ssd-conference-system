e539587a6ccda620a8cc8b94fde533d5be96f71e
szalo13 <kamil_sza@yahoo.pl>
2017-06-06 21:09:00 +0200

    Changing auth provider

--- frontend/src/providers/auth/auth.ts
+++ frontend/src/providers/auth/auth.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+import { Angular2TokenService } from 'angular2-token';
+import { Subject, Observable } from 'rxjs';
+
 import 'rxjs/add/operator/map';
 
 /*
@@ -11,8 +14,45 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello AuthProvider Provider');
+  userSignedIn$:Subject<boolean> = new Subject();
+
+  constructor(public authService:Angular2TokenService) {
+    this.authService.validateToken().subscribe(
+      res => res.status == 200 ? this.userSignedIn$.next(res.json().success): this.userSignedIn$.next(false)
+    )
   }
 
+  logOutUser():Observable<Response>{
+
+    return this.authService.signOut().map(
+        res => {
+          this.userSignedIn$.next(false);
+          return res;
+        }
+    );
+  }
+
+  registerUser(signUpData:  {
+    email:string,
+    password:string,
+    passwordConfirmation:string}):Observable<Response>{
+     return this.authService.registerAccount(signUpData).map(
+         res => {
+           this.userSignedIn$.next(true);
+           return res
+         }
+     );
+   }
+
+   logInUser(signInData: {
+     email:string,
+     password:string}):Observable<Response>{
+
+     return this.authService.signIn(signInData).map(
+         res => {
+           this.userSignedIn$.next(true);
+           return res
+         }
+     );
+   }
 }
